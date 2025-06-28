9a69a4a0807770e47a3064babb7159fe5fddfed0
Aadilsamar786 <aadilsamar50@gmail.com>
2025-06-29 02:09:06 +0530

    Add files via upload

---
+++ Online Voting System/src/test/java/vote/com/servlet/Online.java
@@ -0,0 +1,13 @@
+package vote.com.servlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Online {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
